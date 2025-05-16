1fdf2e1f8fd82f804367c857ecf42559a7a7c9aa
adam <aattia1ot@gmail.com>
2025-05-15 22:47:29 -0400

    feat: add sample application image and getResourceAsStream() to load image

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("JavaFX App");
+
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/resources/icon.png")));
         primaryStage.show();
     }
 
